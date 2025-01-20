753acd6d38ca0b7f72ec6962b67e156966d713b6
Kawã Kleber <kawaklebersc@gmail.com>
2025-01-20 15:46:40 -0300

    Commit

---
+++ JS/Butons/butttonRedirect.js
@@ -0,0 +1,23 @@
+//Primeiro Link
+const btnLink = document.getElementById('explore')
+btnLink.addEventListener('click', () => {
+    const url = 'https://pilotoewebprojeto.netlify.app/'
+    window.open(url, '_blank')
+})
+
+//Segundo Link
+const btnLink2 = document.getElementById('explore2')
+
+btnLink2.addEventListener('click', () => {
+    const url = 'https://pilotoone.netlify.app/'
+    window.open(url, '_blank')
+})
+
+//Terceiro Link
+
+const btnLink3 = document.getElementById('explore3')
+
+btnLink3.addEventListener('click', () => {
+    const url = 'https://pilotothree.netlify.app/'
+    window.open(url, '_blank')
+})
